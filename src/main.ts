cb48693081f5f1de86d670bc4c45eb3949d56e13
Washington Irving <iamwashingyou@gmail.com>
2024-09-22 15:07:46 +0300

    configure biome

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-console.log("blacksmith")
+console.log("blacksmith");
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
+// biome-ignore lint/style/noDefaultExport: <explanation>
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     css: {
         modules: {
-            localsConvention: 'camelCaseOnly',
+            localsConvention: "camelCaseOnly",
         },
     },
 });
