c1d02a536e6d3f4d11f948d830871897ffaca980
Washington Irving <iamwashingyou@gmail.com>
2024-09-22 17:43:41 +0300

    fix lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-// biome-ignore lint/correctness/noNodejsModules: Config file
-import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
