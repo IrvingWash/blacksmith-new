dccf98feb7f7dda07fd826f416f2cf78da234cf7
Washington Irving <iamwashingyou@gmail.com>
2024-09-22 14:40:15 +0300

    finish

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    css: {
+        modules: {
+            localsConvention: 'camelCaseOnly',
+        },
+    },
 });
