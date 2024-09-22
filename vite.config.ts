3bc7eb1ebabd81ecdca33129f2d274f9bcce2fe4
Washington Irving <iamwashingyou@gmail.com>
2024-09-22 15:20:02 +0300

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
-// biome-ignore lint/style/noDefaultExport: <explanation>
+// biome-ignore lint/style/noDefaultExport: Config file
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     css: {
