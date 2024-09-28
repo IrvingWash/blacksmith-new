91c288b0e61337f56e6095f8462714844e957aaa
Washington Irving <iamwashingyou@gmail.com>
2024-09-28 16:44:39 +0300

    configure deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // biome-ignore lint/style/noDefaultExport: Config file
 export default defineConfig({
+    base: "/blacksmith/",
     plugins: [react(), tsconfigPaths()],
     css: {
         modules: {
